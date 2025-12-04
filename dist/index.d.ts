51ac58d70e1c5bed1af2ce70d4cf9695c0f4e1da
Bruno Eduardo Santos <bruno.e10@aluno.ifsc.edu.br>
2025-12-04 16:11:56 -0300

    chore: prepare release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,5 @@
-export { Glpi } from './nodes/Glpi/Glpi.node';
-export { GlpiApi } from './nodes/Glpi/GlpiApi.credentials';
+import { Glpi } from './nodes/Glpi/Glpi.node';
+import { GlpiApi } from './nodes/Glpi/GlpiApi.credentials';
+export { Glpi, GlpiApi };
+export declare const nodes: (typeof Glpi)[];
+export declare const credentials: (typeof GlpiApi)[];
--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-export { Glpi } from './nodes/Glpi/Glpi.node';
-export { GlpiApi } from './nodes/Glpi/GlpiApi.credentials';
+import { Glpi } from './nodes/Glpi/Glpi.node';
+import { GlpiApi } from './nodes/Glpi/GlpiApi.credentials';
+
+export { Glpi, GlpiApi };
+
+// Exports explícitos que o n8n busca ao carregar pacotes externos
+export const nodes = [Glpi];
+export const credentials = [GlpiApi];
